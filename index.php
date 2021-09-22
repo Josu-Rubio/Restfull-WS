ac99938cf406f9e5017c0a193fb363663e9ea399
Josué Rubio <josu_mr@hotmail.com>
2021-09-22 12:40:34 +0200

    Starting a PHP app

---
+++ index.php
@@ -0,0 +1,9 @@
+<h1>Hello from Josu Rubio HTML Page</h1>
+<p>
+<?php
+   echo "Hi there.\n";
+   $answer = 6 * 7;
+   echo "The answer to life the universe and everything is $answer\n";
+?>
+</p>
+<p>Yes another paragraph.</p>
